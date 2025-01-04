a2dea0891d9491f8e0c22cf24055f36e1e3ec56d
Won <58035906+Trugtie@users.noreply.github.com>
2025-01-04 16:58:26 +0700

    feat: add follow target

---
+++ Assets/_Data/MISC/FollowTarget.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FollowTarget : BaseMonobehaviour
+{
+    [Header(" Elements ")]
+    [SerializeField] protected Transform _target;
+
+    [Header(" Settings ")]
+    [SerializeField] protected float _followSpeed = 7f;
+
+    protected virtual void FixedUpdate()
+    {
+        Following();
+    }
+
+    protected virtual void Following()
+    {
+        transform.position = Vector3.Lerp(transform.position, _target.position, _followSpeed * Time.fixedDeltaTime);
+    }
+}
