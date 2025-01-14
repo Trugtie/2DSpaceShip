a684e2f62ec7ee57a96260771adf3c6ff90aaeff
Won <58035906+Trugtie@users.noreply.github.com>
2025-01-14 22:57:15 +0700

    feat: modified ship rotate speed

--- Assets/_Data/GameScripts/Object/ObjectMovement.cs
+++ Assets/_Data/GameScripts/Object/ObjectMovement.cs
@@ -6,6 +6,7 @@ public class ObjectMovement : BaseMonobehaviour
     [Header(" ShipMovement Settings ")]
     [SerializeField] protected Vector3 _targetPosition;
     [SerializeField] protected float _maxSpeed = 0.01f;
+    [SerializeField] protected float _rotateSpeed = 10f;
 
     protected virtual void FixedUpdate()
     {
@@ -17,6 +18,7 @@ public class ObjectMovement : BaseMonobehaviour
     {
         base.ResetValue();
         _maxSpeed = 0.01f;
+        _rotateSpeed = 10f;
     }
 
     protected virtual void LookAtTargetPosition()
@@ -25,8 +27,9 @@ public class ObjectMovement : BaseMonobehaviour
         Vector2 direction = (Vector2)_targetPosition - currentPosition;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
 
-        transform.parent.rotation = Quaternion.Euler(0, 0, angle);
+        transform.parent.rotation = Quaternion.Lerp(transform.parent.rotation, targetRotation, _rotateSpeed * Time.fixedDeltaTime);
     }
 
     protected virtual void Moving()
--- Assets/_Data/GameScripts/Ship/ShipFollowMouse.cs
+++ Assets/_Data/GameScripts/Ship/ShipFollowMouse.cs
@@ -9,6 +9,13 @@ public class ShipFollowMouse : ObjectMovement
         base.FixedUpdate();
     }
 
+    protected override void ResetValue()
+    {
+        base.ResetValue();
+        _maxSpeed = 0.01f;
+        _rotateSpeed = 100f;
+    }
+
     protected void GetMousePosition()
     {
         _targetPosition = InputManager.Instance.MousePosition;
