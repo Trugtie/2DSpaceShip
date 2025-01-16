ae909993bb0b4bafbf4c5cc755a8a5843b8b5ac5
Won <58035906+Trugtie@users.noreply.github.com>
2025-01-16 23:13:55 +0700

    feat: add wrap ability

--- Assets/_Data/GameScripts/Abilities/AbilityWarp.cs
+++ Assets/_Data/GameScripts/Abilities/AbilityWarp.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 public class AbilityWarp : BaseAbility
 {
     [Header(" AbilityWarp Elements ")]
+    [SerializeField] protected bool _isWarping = false;
     [SerializeField] protected Vector4 _warpDirection;
-    [SerializeField] protected bool _isWarping;
+
+    [Header(" AbilityWarp Settings ")]
+    [SerializeField] protected float _warpSpeed = 1f;
+    [SerializeField] protected float _warpDistance = 2f;
     protected Vector4 _keyDirection;
 
     protected override void Update()
     {
         base.Update();
+        CheckKeyWarping();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
         Warping();
     }
 
-    protected virtual void Warping()
+    protected virtual void CheckKeyWarping()
     {
         if (!_isReady) return;
         if (_isWarping) return;
@@ -44,5 +54,46 @@ public class AbilityWarp : BaseAbility
     {
         Debug.Log("Down");
         _warpDirection.w = 1;
+    }
+
+    protected virtual void Warping()
+    {
+        if (_isWarping) return;
+        if (IsWarpDirectionNotSet()) return;
+
+        Debug.Log("Warping");
+        Debug.Log(_warpDirection);
+
+        _isWarping = true;
+
+        Invoke(nameof(WarpFinish), _warpSpeed);
+    }
+
+    protected virtual void WarpFinish()
+    {
+        Debug.Log("Warp Finish");
+        MoveWarp();
+        _warpDirection = Vector4.zero;
+        _isWarping = false;
+        Active();
+    }
+
+    protected virtual void MoveWarp()
+    {
+        Transform currentTransform = Abilities.AbilityObjectCtrl.transform;
+
+        Vector3 newPos = currentTransform.position;
+
+        if (_warpDirection.x == 1) newPos.x -= _warpDistance;
+        if (_warpDirection.y == 1) newPos.x += _warpDistance;
+        if (_warpDirection.z == 1) newPos.y += _warpDistance;
+        if (_warpDirection.w == 1) newPos.y -= _warpDistance;
+
+        currentTransform.position = newPos;
+    }
+
+    protected virtual bool IsWarpDirectionNotSet()
+    {
+        return _warpDirection == Vector4.zero;
     }
 }
